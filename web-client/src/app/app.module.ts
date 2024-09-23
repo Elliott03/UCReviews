5261a73f12ee3a5e9640a38db637530f1b10208b
Adam Tulloss <artulloss@gmail.com>
2024-09-23 18:25:50 -0400

    Angular 18

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -8,41 +8,35 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { DormPageComponent } from './dorm-page/dorm-page.component';
 import { NgxStarsModule } from 'ngx-stars';
 import { AuthGuard } from './core/guards/auth.guard';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginPageComponent,
-    SignupPageComponent,
-    NavbarComponent,
-    DashboardComponent,
-    DormPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    NgxStarsModule,
-    RouterModule.forRoot([
-      { path: 'signup', component: SignupPageComponent},
-      { path: 'login', component: LoginPageComponent},
-      { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-      { path: 'dorm/:dorm', component: DormPageComponent, canActivate: [AuthGuard]},
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-      { path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
-    ])
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
-    AuthGuard
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginPageComponent,
+        SignupPageComponent,
+        NavbarComponent,
+        DashboardComponent,
+        DormPageComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgxStarsModule,
+        RouterModule.forRoot([
+            { path: 'signup', component: SignupPageComponent },
+            { path: 'login', component: LoginPageComponent },
+            { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+            { path: 'dorm/:dorm', component: DormPageComponent, canActivate: [AuthGuard] },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+        ])], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        AuthGuard,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
