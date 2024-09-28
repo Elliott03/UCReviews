7f3f5015492ae9b16729f964829370ddd8d613b1
Elliott Phillips <elliottphillips@Elliotts-MacBook-Air.local>
2024-09-28 14:08:48 -0400

    remove comments

--- web-client/src/app/login-page/login-page.component.ts
+++ web-client/src/app/login-page/login-page.component.ts
@@ -53,46 +53,8 @@ export class LoginPageComponent{
         console.error(error);
       }
     }
-
-    // this.password = this.password.toLowerCase().trim();
-    // if (this.isValidPassword()) {
-    //   try {
-    //     const response = await this._authService.login(this.email, this.password).toPromise();
-    //     if (response) {
-    //       const sub = localStorage.getItem('sub');
-    //       const user = await this._userService.getUserById(Number(sub)).toPromise();
-    //       if (user) {
-    //         localStorage.setItem('user', JSON.stringify(user));
-    //         this._router.navigate(['/dashboard']);
-    //       }
-    //     }
-    //   } catch (error) {
-    //     this.isError = true;
-    //     this.errorMessage = "Incorrect Password";
-    //   }
-    // }
   }
   
-  // isValidPassword(): boolean {
-  //   if (!this.isNumeric(this.password)) {
-  //     this.isError = true;
-  //     this.errorMessage = "Password must be numeric"
-  //     return false;
-  //   }
-  //   if (this.password.length < 6) {
-  //     this.isError = true;
-  //     this.errorMessage = "Password must be 6 characters";
-  //     return false;
-  //   }
-
-  //   this.isError = false;
-  //   this.errorMessage = "";
-  //   return true;
-  // }
-
-  // isNumeric(input: string): boolean {
-  //   return /^[0-9]+$/.test(input);
-  // }
 
   updateErrorMessage() {
 
