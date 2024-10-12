4e35ddb355225b43eb26e76336d9f90231cb514c
Adam Tulloss <artulloss@gmail.com>
2024-10-11 22:03:45 -0400

    Single Dorm Page averages updatedd

--- web-client/src/app/Models/Dorm.ts
+++ web-client/src/app/Models/Dorm.ts
@@ -1,22 +1,29 @@
-import { IReview } from "./Review"
+import { IReview } from './Review';
 
 export interface ILargeDorm {
-    id: number,
-    name: string,
-    style: string,
-    cost: string,
-    description: string,
-    nameQueryParameter: string,
-    reviews: IReview[]
-    averageRating: number,
+  id: number;
+  name: string;
+  style: string;
+  cost: string;
+  description: string;
+  nameQueryParameter: string;
+  reviews: IReview[];
 }
 
 export interface ISmallDorm {
-    id: number,
-    name: string,
-    style: string,
-    cost: string,
-    nameQueryParameter: string,
-    averageRating: number,
-    image: string
+  id: number;
+  name: string;
+  style: string;
+  cost: string;
+  nameQueryParameter: string;
+  averageRating: number;
+  image: string;
+}
+
+export interface ILargeDormWithRating extends ILargeDorm {
+  averageRating: number;
+}
+
+export interface ISmallDormWithRating extends ISmallDorm {
+  averageRating: number;
 }
--- web-client/src/app/core/services/dorm.service.ts
+++ web-client/src/app/core/services/dorm.service.ts
@@ -1,22 +1,36 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { ILargeDorm, ISmallDorm } from "src/app/Models/Dorm";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import {
+  ILargeDorm,
+  ILargeDormWithRating,
+  ISmallDorm,
+} from 'src/app/Models/Dorm';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DormService {
-    constructor(
-        private _http: HttpClient
-    ) {}
+  constructor(private _http: HttpClient) {}
 
-    getDorms(): Observable<ISmallDorm[]> {
-        return this._http.get<ISmallDorm[]>('/api/Dorm');
-    }
+  getDorms(): Observable<ISmallDorm[]> {
+    return this._http.get<ISmallDorm[]>('/api/Dorm');
+  }
 
-    getDorm(queryParam: string): Observable<ILargeDorm> {
-        return this._http.get<ILargeDorm>(`/api/Dorm/${queryParam}`);
-    }
+  getDorm(queryParam: string): Observable<ILargeDormWithRating> {
+    const dorm = this._http.get<ILargeDorm>(`/api/Dorm/${queryParam}`);
+    return dorm.pipe(
+      map((dorm) => {
+        const sum = dorm.reviews.reduce(
+          (sum, review) => sum + review.starRating,
+          0
+        );
+        const averageRating = sum / dorm.reviews.length;
+        return {
+          ...dorm,
+          averageRating,
+        };
+      })
+    );
+  }
 }
