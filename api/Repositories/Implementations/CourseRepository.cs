bb90ad5679a68eaae60946eeb80cd6df439389ee
whsiq <staggsce@mail.uc.edu>
2025-02-08 21:56:29 -0500

    Created initial course backend

---
+++ api/Repositories/Implementations/CourseRepository.cs
@@ -0,0 +1,52 @@
+﻿using api.Models;
+using api.Repositories.Interfaces;
+using api.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Linq.Expressions;
+
+namespace api.Repositories.Implementations;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly UCReviewsContext _dbContext;
+    private readonly PaginationSettings _paginationSettings;
+
+    public CourseRepository(UCReviewsContext dbContext, IOptions<PaginationSettings> paginationSettings)
+    {
+        _dbContext = dbContext;
+        _paginationSettings = paginationSettings.Value;
+    }
+
+    public async Task SaveCourse(Course course)
+    {
+        await _dbContext.AddAsync(course);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Course> GetCourse(int id)
+    {
+        return await HandleGetCourse(c => c.Id == id);
+    }
+
+    public async Task<Course> GetCourse(string slug)
+    {
+        return await HandleGetCourse(c => c.NameQueryParameter == slug);
+    }
+
+    public async Task<IEnumerable<Course>> GetCourses(int prev, int perPage)
+    {
+        var query = _dbContext.Course.AsQueryable();
+        perPage = int.Min(perPage, _paginationSettings.MaxPerPage);
+        query = query.Where(c => c.Id > prev).Take(perPage);
+        query = query.Include(c => c.ReviewSummary);
+        return await query.ToListAsync();
+    }
+
+    private async Task<Course> HandleGetCourse(Expression<Func<Course, bool>> predicate)
+    {
+        var query = _dbContext.Course.AsQueryable();
+        query = query.Include(c => c.ReviewSummary);
+        return await query.Where(predicate).FirstOrDefaultAsync();
+    }
+}
---
+++ api/Services/Implementations/CourseService.cs
@@ -0,0 +1,34 @@
+﻿using api.Models;
+using api.Repositories.Interfaces;
+using api.Services.Interfaces;
+
+namespace api.Services.Implementations;
+public class CourseService : ICourseService
+{
+    private readonly ICourseRepository _courseRepository;
+
+    public CourseService(ICourseRepository courseRepository)
+    {
+        _courseRepository = courseRepository;
+    }
+
+    public async Task<Course> GetCourse(int id)
+    {
+        return await _courseRepository.GetCourse(id);
+    }
+
+    public async Task<Course> GetCourse(string slug)
+    {
+        return await _courseRepository.GetCourse(slug);
+    }
+
+    public async Task<IEnumerable<Course>> GetCourses(int prev, int perPage)
+    {
+        return await _courseRepository.GetCourses(prev, perPage);
+    }
+
+    public async Task SaveCourse(Course course)
+    {
+        await _courseRepository.SaveCourse(course);
+    }
+}
