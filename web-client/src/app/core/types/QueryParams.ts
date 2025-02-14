91329e5925f868c5f099ffc862df665c32f641ec
Pratik <101081557+pratxks@users.noreply.github.com>
2025-02-14 09:41:57 -0500

    QueryParams

--- web-client/src/app/core/types/QueryParams.ts
+++ web-client/src/app/core/types/QueryParams.ts
@@ -1,11 +1,7 @@
-// /src/app/types/query-params.ts
-
-// Define PageableQueryParam with searchTerm as an optional field
 export type PageableQueryParam = {
   prev: number;
   perPage: number;
-  searchTerm?: string; // Add searchTerm as an optional field
+  searchTerm?: string;
 };
 
-// QueryParams now references the updated PageableQueryParam
 export type QueryParams = PageableQueryParam;
