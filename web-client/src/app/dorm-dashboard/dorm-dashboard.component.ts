eee6ae76a16e6aa2cf1ee976611697cb7e4b228d
Pratik <101081557+pratxks@users.noreply.github.com>
2025-02-14 08:07:23 -0500

    Added search functionality for residence halls

--- web-client/src/app/dorm-dashboard/dorm-dashboard.component.ts
+++ web-client/src/app/dorm-dashboard/dorm-dashboard.component.ts
@@ -12,26 +12,36 @@ import { AuthService } from '../core/services/auth.service';
 export class DormDashboardComponent implements OnInit {
   hasChildRoute = false;
   dorms: ISmallDorm[] = [];
+  searchTerm: string = ''; // New property for search
   prev = 0;
   perPage = 6;
+
   constructor(
     private _dormService: DormService,
     private _router: Router,
     public _authService: AuthService,
     private _route: ActivatedRoute
   ) {}
+
   ngOnInit(): void {
     if (this._authService.isLoggedIn()) {
       this.loadDorms();
     } else {
       this._router.navigate(['/signup']);
     }
+
     this._router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.hasChildRoute = this._route.children.length > 0;
+
+        // Only clear search term if navigating to a specific dorm page
+        if (this._router.url.includes('/dashboard/housing/')) {
+          this.searchTerm = '';  // Clear the search term
+        }
       }
     });
   }
+
   loadDorms() {
     this._dormService
       .getDorms({
@@ -43,14 +53,22 @@ export class DormDashboardComponent implements OnInit {
         this.dorms.push(...dorms);
       });
   }
+
   getDormRatingTitle(dorm: ISmallDorm) {
     if (!dorm.reviewSummary?.averageRating) {
       return 'Not yet rated';
     }
     return `${dorm.reviewSummary.averageRating} stars`;
   }
+
   onScroll(): void {
     this.prev += this.perPage;
     this.loadDorms();
+  }
+
+  filteredDorms(): ISmallDorm[] {
+    return this.dorms.filter(dorm =>
+      dorm.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
