dde90a3fc1cf862defbbc391e0d91dc689023957
whsiq <staggsce@mail.uc.edu>
2025-03-02 13:58:27 -0500

    add-course-modal refactor to use form control

--- web-client/src/app/add-course-modal/add-course-modal.component.ts
+++ web-client/src/app/add-course-modal/add-course-modal.component.ts
@@ -1,63 +1,114 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Course, ICourse } from '../Models/Course';
 import { CourseService } from '../core/services/course.service';
 import { Router } from '@angular/router';
+import { Filter } from 'bad-words';
+import { AbstractControl, FormControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-add-course-modal',
   templateUrl: './add-course-modal.component.html',
-  styleUrls: ['./add-course-modal.component.scss']
+  styleUrls: ['./add-course-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddCourseModalComponent {
   course?: ICourse;
-  courseName: string = '';
-  courseSubject: string = '';
-  courseNumber: string = '';
-  courseNameError: string = '';
-  courseSubjectError: string = '';
-  courseNumberError: string = '';
-  errorMessage: string = '';
   isLoading: boolean = false;
+  errorMessage: string = '';
+  filter: Filter = new Filter();
+
+  readonly courseSubject = new FormControl('', [
+    Validators.required, 
+    Validators.pattern(/^[a-zA-Z]{2,4}$/),
+    this.profanityValidator()    
+  ]);
+  readonly courseName = new FormControl('', [Validators.required, this.profanityValidator()]);
+  readonly courseNumber = new FormControl('', [Validators.required, Validators.pattern(/^\d{4}[a-zA-Z]?$/)]);
+
+  courseSubjectError = signal('');
+  courseNameError = signal('');
+  courseNumberError = signal('');
 
   constructor(
     public dialogRef: MatDialogRef<AddCourseModalComponent>,
     private _courseService: CourseService,
     private router: Router
-  ) {}
+  ) {
+    merge(this.courseSubject.statusChanges, this.courseSubject.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateSubjectError());
+    merge(this.courseName.statusChanges, this.courseName.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateNameError());
+    merge(this.courseNumber.statusChanges, this.courseNumber.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateNumberError());
+  }
+
+  profanityValidator(): ValidatorFn {
+    return (control:AbstractControl): ValidationErrors | null => {
+      const filter = new Filter();
+      return filter.isProfane(control.value) ? {profanity: true} : null;
+    };
+  }
+
+  updateSubjectError() {
+    if (this.courseSubject.hasError('required')) {
+      this.courseSubjectError.set('Course subject is required.');
+    }
+    else if (this.courseSubject.hasError('pattern')) {
+      this.courseSubjectError.set('Course subject must contain 2-4 alphabetic letters.');
+    }
+    else if (this.courseSubject.hasError('profanity')) {
+      this.courseSubjectError.set('Course subject contains inappropriate language.');
+    }
+    else {
+      this.courseSubjectError.set('');
+    }
+  }
+
+  updateNameError() {
+    if (this.courseName.hasError('required')) {
+      this.courseNameError.set('Course name is required.');
+    }
+    else if (this.courseName.hasError('profanity')) {
+      this.courseNameError.set('Course name contains inappropriate language.');
+    }
+    else {
+      this.courseNameError.set('');
+    }
+  }
+
+  updateNumberError()  {
+    if (this.courseNumber.hasError('required')) {
+      this.courseNumberError.set('Course number is required.');
+    }
+    else if (this.courseNumber.hasError('pattern')) {
+      this.courseNumberError.set('Course number must contain 4 digits followed by an optional letter.');
+    }
+    else {
+      this.courseNumberError.set('');
+    }
+  }
 
   onCancel(): void {
     this.dialogRef.close();
   }
 
   onAddCourse(): void {
-    this.courseSubjectError = '';
-    this.courseNumberError = '';
-    this.errorMessage = '';
-
-    if (!this.courseName || !this.courseSubject || !this.courseNumber) {
-      this.errorMessage = 'All fields are required.';
-    }
-
-    const subject: string = this.courseSubject.trim().toUpperCase();
-    const number: string = this.courseNumber.trim().toUpperCase();
-    const name: string = this.courseName.trim().toUpperCase();
-
-    const courseNumberRegex = /^\d{4}[a-zA-Z]?$/;
-    if (!courseNumberRegex.test(number)) {
-      this.courseNumberError = 'Course number must contain four digits and one optional alphabetic letter.';
-    }
-
-    const courseSubjectRegex = /^[a-zA-Z]{2,4}$/;
-    if (!courseSubjectRegex.test(subject)) {
-      this.courseSubjectError = 'Course subject must contain 2-4 alphabetic letters.';
-    }
-
-    if (this.courseNumberError || this.courseSubjectError || this.errorMessage) {
+    if (this.courseSubject.invalid || this.courseName.invalid || this.courseNumber.invalid || 
+      !this.courseSubject.value || !this.courseName.value || !this.courseNumber.value) {
       return;
     }
 
     this.isLoading = true;
+
+    const subject = this.courseSubject.value.toUpperCase();
+    const name = this.courseName.value.toUpperCase();
+    const number = this.courseNumber.value.toUpperCase();
 
     const course: ICourse = this.course = {
       id: 0,
@@ -81,6 +132,4 @@ export class AddCourseModalComponent {
       }
     });
   }
-  // TODO:
-  //       find a way to ensure there aren't any curse words in the course subject or name
 }
